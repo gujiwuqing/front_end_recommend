a46693ef367d0e4ad595b1d4543ad43cba031056
孤寂无情 <15279279713@163.com>
2022-04-13 23:49:34 +0800

    掘金接口请求

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,25 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import {useEffect, useState} from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  useEffect(() => {
+    axios.post('https://api.juejin.cn/recommend_api/v1/article/recommend_all_feed?aid=2608&uuid=6996871449340708367', {
+      data: {
+        client_type: 2608,
+        cursor: "0",
+        id_type: 2,
+        limit: 20,
+        sort_type: 300
+      }
+    }).then(res=>{
+      console.log(res);
+    })
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <h2>前端每日推荐文章</h2>
     </div>
   )
 }
